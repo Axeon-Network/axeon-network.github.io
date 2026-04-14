505c6d77fd236744629746dba2a707cea71047c8
AveryEclipse <thekayaurora@gmail.com>
2026-04-14 20:06:55 +0300

    integrate averywebsite changes, remove unnecessary stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import remarkBreaks from 'remark-breaks'; /* i shouldnt need to use a PLUGIN just to properly do a hard break ffs */
+import icon from "astro-icon";
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [],
+  integrations: [icon()],
   site: "https://axeon-network.github.io",
   markdown: {
     remarkPlugins: [remarkBreaks],
--- src/config.ts
+++ src/config.ts
@@ -6,11 +6,7 @@ export const d = {
   shortname: "Axeon", // Shorter form of the name of the owner of the site, Mostly used in introductions or as an alternative to the longer name.
   favicon: "/img/logotype.png", // The site's favicon
   logo: "/img/logo.png", // The site's logo
+
   copyrightyear: "2017-2026", // Copyright year. Change at first site change of the year
   sourcecode: "Axeon-Network/axeon-network.github.io", // Location of the published source code, in format "username/repo". It has to be GitHub.
-
-  pronouns: "", // Your pronouns
-  tz: "", // Your timezone
-
-  faver: "7.1.0", // The version of FontAwesome to use, for some of the icons
 };
